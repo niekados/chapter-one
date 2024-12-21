321c44f0270c3d76fcd9fd9235a2e325802f2885
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-12-21 10:22:34 +0000

    feat: add sorting by category

--- books/views.py
+++ books/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Book
+from .models import Book, Genre
 
 
 def all_books(request):
     """ A view to show a list of all books """
 
     books = Book.objects.all()
+    genres = Genre.objects.all()
     query = None
+    selected_genre = None
 
+    # Categories
     if request.GET:
+        if 'genre' in request.GET:
+            selected_genre = request.GET['genre']
+            books = books.filter(genre__name=selected_genre)
+
+        # Search functionality
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -26,7 +34,9 @@ def all_books(request):
 
     context = {
         'books': books,
-        'search_keyword': query
+        'genres': genres,
+        'search_keyword': query,
+        'current_genre': selected_genre,
     }
 
     return render(request, 'books/books_list.html', context)
