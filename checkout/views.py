e129d4742f6462dff0f1f73dc7a5c154a2b9a4e9
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-12-28 21:16:48 +0000

    fet: add checkout data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -6,6 +9,24 @@ from books.models import Book
 from cart.contexts import cart_contents
 from django.conf import settings
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Oops! Something went wrong with your \
+            payment. Please give it another try in a moment.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
