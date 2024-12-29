bfd000b97b97af8ceb494a84bc0f71b1e2d3bf18
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-12-29 19:32:12 +0000

    Feat: add CountryField to Order model and update form to match it

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from uuid import uuid4
 from django.db.models import Sum
+from django_countries.fields import CountryField
 from books.models import Book
 
 
@@ -13,7 +14,9 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=20, null=False, blank=False)
 
     # Address Details
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(
+        blank_label='Country *', null=False, blank=False
+    )
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
